55f09c8028bf3e2233abc87fda6e9e5560ac39d6
UlascanErsoy <ulas.742@gmail.com>
2023-11-26 20:32:34 -0600

    updated some scripts to work with my setup

---
+++ battery/battery.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3 
+"""github.com/UlascanErsoy
+Battery indicator. Who uses perl anymore.
+"""
+
+import subprocess
+
+def get_battery_glyph(percent):
+    perc = percent.strip().replace("%","")
+    if not perc.isnumeric():
+        return ""
+
+    perc = int(perc)
+
+    if 85 <= perc <= 100:
+        return "", ""
+    elif 75 <= perc < 90:
+        return "", ""
+    elif 50 <= perc < 75:
+        return "", ""
+    elif 25 <= perc < 50:
+        return "", "#F5A442"
+    else:
+        return "", "#DD4444"
+if __name__ == "__main__":
+
+    out,err= subprocess.Popen(["acpi","-b"],stdout=subprocess.PIPE)\
+                        .communicate()
+
+    battery = [item for item in out.decode().split("\n") 
+               if not "unavailable" in item]
+
+    if not battery:
+        print("NO BAT")
+        print("")
+        print("#FF0000")
+
+    status = battery[0].split(":")[-1]
+
+    status, percent = status.split(",")[:2]
+    icon, color = get_battery_glyph(percent)
+
+    if status.strip() == "Discharging":
+        print(f"{icon}{percent}")
+        print("")
+        print(color)
+    elif status.strip() == "charging":
+        print(f"⚡{percent}")
+    elif status.strip() == "Full":
+        print("")
+        print("")
+        print("#55AA55")
---
+++ calendar/google-calendar.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+if [ "$BLOCK_BUTTON" == "1" ]; then
+    /usr/bin/google-chrome --new-window calendar.google.com&
+fi
+
+./calendar
